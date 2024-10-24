aff97b9e717ba215487435204d5da1ede63d706a
Kiran Joshi <keronz.joshi@gmail.com>
2024-10-24 16:14:41 +0545

    buy_screen - scrollable

--- lib/Screens/Inside Screens/buy_screen.dart
+++ lib/Screens/Inside Screens/buy_screen.dart
@@ -25,9 +25,9 @@ class _BuyScreenState extends State<BuyScreen> {
         _totalPrice = total.toStringAsFixed(2);
       });
     }
-}
+  }
 
-Widget _buildDetailRow(String label, String value, {bool isBold = false}) {
+  Widget _buildDetailRow(String label, String value, {bool isBold = false}) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
       child: Row(
@@ -48,7 +48,7 @@ Widget _buildDetailRow(String label, String value, {bool isBold = false}) {
         ],
       ),
     );
-}
+  }
 
   void _reset() {
     _formKey.currentState?.reset(); // Reset form and validation state
@@ -65,101 +65,104 @@ Widget _buildDetailRow(String label, String value, {bool isBold = false}) {
       // appBar: AppBar(
       //   title: const Text('Buy Screen'),
       // ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                controller: _priceController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Buy Price / Per Share',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid price';
-                  }
-                  if (double.tryParse(value) == null) {
-                    return 'Enter a valid number';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _quantityController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Quantity',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid quantity';
-                  }
-                  if (int.tryParse(value) == null) {
-                    return 'Enter a valid integer';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: _calculate,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            8.0), // Adjust the radius for slight rounding
-                      ),
-                    ),
-                    child: const Text(
-                      'Calculate',
-                      style: TextStyle(color: Colors.white),
-                    ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: _priceController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Buy Price / Per Share',
                   ),
-                  ElevatedButton(
-                    onPressed: _reset,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            8.0), // Slight rounding for the border radius
-                      ),
-                    ),
-                    child: const Text(
-                      'Reset',
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a valid price';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Enter a valid number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _quantityController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Quantity',
                   ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              if (_totalPrice.isNotEmpty) 
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a valid quantity';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Enter a valid integer';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const SizedBox(
-                      height: 30.0,
+                    ElevatedButton(
+                      onPressed: _calculate,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              8.0), // Adjust the radius for slight rounding
+                        ),
+                      ),
+                      child: const Text(
+                        'Calculate',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                    _buildDetailRow(
-                        "Purchase Value",
-                        (double.parse(_priceController.text) *
-                                int.parse(_quantityController.text))
-                            .toStringAsFixed(2)),
-                    _buildDetailRow("Broker Commission", "16.56"),
-                    _buildDetailRow("SEBON Commission", "0.69"),
-                    _buildDetailRow("DP Charge", "25"),
-                    const Divider(),
-                    _buildDetailRow("Total Amount", _totalPrice, isBold: true),
+                    ElevatedButton(
+                      onPressed: _reset,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              8.0), // Slight rounding for the border radius
+                        ),
+                      ),
+                      child: const Text(
+                        'Reset',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
                   ],
                 ),
-            ],
+                const SizedBox(height: 16),
+                if (_totalPrice.isNotEmpty)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      _buildDetailRow(
+                          "Purchase Value",
+                          (double.parse(_priceController.text) *
+                                  int.parse(_quantityController.text))
+                              .toStringAsFixed(2)),
+                      _buildDetailRow("Broker Commission", "16.56"),
+                      _buildDetailRow("SEBON Commission", "0.69"),
+                      _buildDetailRow("DP Charge", "25"),
+                      const Divider(),
+                      _buildDetailRow("Total Amount", _totalPrice,
+                          isBold: true),
+                    ],
+                  ),
+              ],
+            ),
           ),
         ),
       ),
