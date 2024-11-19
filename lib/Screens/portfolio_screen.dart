f6ec43b9b5e5f793f4e7a43be6dc5c8ddbd7f964
Kiran Joshi <keronz.joshi@gmail.com>
2024-11-19 17:01:21 +0545

    dsdas

--- lib/Screens/portfolio_screen.dart
+++ lib/Screens/portfolio_screen.dart
@@ -1,13 +1,80 @@
 import 'package:flutter/material.dart';
 
-class PortfolioScreen extends StatelessWidget {
+class PortfolioScreen extends StatefulWidget {
   static const String portfolioScreenRoute = '/portfolio';
-    
+  
+  @override
+  _PortfolioScreenState createState() => _PortfolioScreenState();
+}
+
+class _PortfolioScreenState extends State<PortfolioScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 6, vsync: this); // 6 tabs
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Portfolio')),
-      body: Center(child: Text('Portfolio Screen')),
+      body: Column(
+        children: [
+          // Making the TabBar scrollable horizontally
+          TabBar(
+            controller: _tabController,
+            isScrollable: true, // Allows horizontal scrolling
+            tabs: const [
+              Tab(
+                  child: Text('My Portfolio',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold))),
+              Tab(
+                  child: Text('Allocation',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold))),
+              Tab(
+                  child: Text('Add Shares',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold))),
+              Tab(
+                  child: Text('History',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold))),
+              Tab(
+                  child: Text('Profit & Loss',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold))),
+              Tab(
+                  child: Text('Import',
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold))),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                Center(child: Text('My Portfolio Screen')),
+                Center(child: Text('Allocation Screen')),
+                Center(child: Text('Add Shares Screen')),
+                Center(child: Text('History Screen')),
+                Center(child: Text('Profit & Loss Screen')),
+                Center(child: Text('Import Screen')),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
