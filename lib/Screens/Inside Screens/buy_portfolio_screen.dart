479330f5776abff5b691ce1cb0fdb81f9d2122a1
Kiran Joshi <keronz.joshi@gmail.com>
2024-11-24 15:28:28 +0545

    ad to bs working

--- lib/Screens/Inside Screens/buy_portfolio_screen.dart
+++ lib/Screens/Inside Screens/buy_portfolio_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nepali_date_picker/nepali_date_picker.dart'; // Add this package in pubspec.yaml
 
 class BuyPortfolioScreen extends StatefulWidget {
   const BuyPortfolioScreen({super.key});
@@ -10,7 +11,10 @@ class BuyPortfolioScreen extends StatefulWidget {
 class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _shareholderController = TextEditingController();
-  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _dateController =
+      TextEditingController(); // For AD date
+  final TextEditingController _nepaliDateController =
+      TextEditingController(); // For BS date
   String? _selectedCompany;
   final TextEditingController _quantityController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
@@ -35,14 +39,12 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
                 controller: _shareholderController,
                 decoration: const InputDecoration(
                   hintText: 'Shareholder name',
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10, // Adjusted for smaller height
-                    horizontal: 10,
-                  ),
-                  isDense: true, // Reduces overall height
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  isDense: true,
                   border: OutlineInputBorder(),
                 ),
-                style: const TextStyle(fontSize: 14), // Smaller font size
+                style: const TextStyle(fontSize: 14),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter the shareholder name';
@@ -62,19 +64,46 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
                   child: TextFormField(
                     controller: _dateController,
                     decoration: InputDecoration(
-                      hintText: 'YYYY-MM-DD',
+                      hintText: 'YYYY-MM-DD (AD)',
                       prefixIcon: const Icon(Icons.calendar_today, size: 20),
                       contentPadding: const EdgeInsets.symmetric(
-                        vertical: 8, // Adjusted for smaller height
-                        horizontal: 10,
-                      ),
-                      isDense: true, // Reduces overall height
+                          vertical: 8, horizontal: 10),
+                      isDense: true,
                       border: const OutlineInputBorder(),
                     ),
-                    style: const TextStyle(fontSize: 14), // Smaller font size
+                    style: const TextStyle(fontSize: 14),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please select a date';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Purchase Date (BS)',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              GestureDetector(
+                onTap: _pickNepaliDate,
+                child: AbsorbPointer(
+                  child: TextFormField(
+                    controller: _nepaliDateController,
+                    decoration: InputDecoration(
+                      hintText: 'YYYY-MM-DD (BS)',
+                      prefixIcon: const Icon(Icons.calendar_today, size: 20),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 10),
+                      isDense: true,
+                      border: const OutlineInputBorder(),
+                    ),
+                    style: const TextStyle(fontSize: 14),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a Nepali date';
                       }
                       return null;
                     },
@@ -98,14 +127,12 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
                 decoration: const InputDecoration(
                   hintText: 'Search for company',
                   prefixIcon: Icon(Icons.search, size: 20),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10, // Adjusted for smaller height
-                    horizontal: 10,
-                  ),
-                  isDense: true, // Reduces overall height
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  isDense: true,
                   border: OutlineInputBorder(),
                 ),
-                style: const TextStyle(fontSize: 14), // Smaller font size
+                style: const TextStyle(fontSize: 14),
                 onChanged: (value) {
                   setState(() {
                     _selectedCompany = value;
@@ -129,14 +156,12 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
                   hintText: 'Enter quantity',
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10, // Adjusted for smaller height
-                    horizontal: 10,
-                  ),
-                  isDense: true, // Reduces overall height
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  isDense: true,
                   border: OutlineInputBorder(),
                 ),
-                style: const TextStyle(fontSize: 14), // Smaller font size
+                style: const TextStyle(fontSize: 14),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter the quantity';
@@ -155,14 +180,12 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
                   hintText: 'Enter purchase price',
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10, // Adjusted for smaller height
-                    horizontal: 10,
-                  ),
-                  isDense: true, // Reduces overall height
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  isDense: true,
                   border: OutlineInputBorder(),
                 ),
-                style: const TextStyle(fontSize: 14), // Smaller font size
+                style: const TextStyle(fontSize: 14),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter the purchase price';
@@ -174,9 +197,8 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
                   onPressed: _submitForm,
                   child: const Text(
                     'Add Share',
@@ -201,13 +223,30 @@ class _BuyPortfolioScreenState extends State<BuyPortfolioScreen> {
     if (picked != null) {
       setState(() {
         _dateController.text = picked.toString().split(' ')[0];
+        _nepaliDateController.text = NepaliDateFormat("yyyy-MM-dd")
+            .format(NepaliDateTime.fromDateTime(picked));
+      });
+    }
+  }
+
+  Future<void> _pickNepaliDate() async {
+    final NepaliDateTime? picked = await showMaterialDatePicker(
+      context: context,
+      initialDate: NepaliDateTime.now(),
+      firstDate: NepaliDateTime(2000, 1, 1),
+      lastDate: NepaliDateTime(2090, 12, 30),
+    );
+    if (picked != null) {
+      setState(() {
+        _nepaliDateController.text =
+            NepaliDateFormat("yyyy-MM-dd").format(picked);
+        _dateController.text = picked.toDateTime().toString().split(' ')[0];
       });
     }
   }
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
-      // Handle form submission
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Share added successfully!')),
       );
